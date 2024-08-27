9bd0e42094c2e68adabac75686b606a2fc854baf
jun-oishi <oishi.junnosuke@gmail.com>
2024-08-28 08:36:03 +0900

    remove unused

--- src/SpectraSpark/util/__init__.py
+++ src/SpectraSpark/util/__init__.py
@@ -3,14 +3,12 @@
 
 import numpy as np
 from typing import TypeAlias, Union
-from SpectraSpark.util.io import listFiles, CsvFile, loadCsv, write_json
+from SpectraSpark.util.io import listFiles, write_json
 
 ArrayLike: TypeAlias = Union[list[float], tuple[float, ...], np.ndarray]
 
 __all__ = [
     "ArrayLike",
     "listFiles",
-    "CsvFile",
-    "loadCsv",
     "write_json",
 ]
